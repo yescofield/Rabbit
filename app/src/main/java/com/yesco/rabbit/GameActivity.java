60bbf5e403c8d2213df340a4d0709ae963ff30fc
yejihuang209592 <yejihuang209592@sogou-inc.com>
2018-12-07 20:17:22 +0800

    优化兔子动画

--- app/src/main/java/com/yesco/rabbit/GameActivity.java
+++ app/src/main/java/com/yesco/rabbit/GameActivity.java
@@ -3,14 +3,14 @@ package com.yesco.rabbit;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.yesco.rabbit.view.GameView;
+import com.yesco.rabbit.view.RabbitSurfaceView;
 
 public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new GameView(this));
+        setContentView(new RabbitSurfaceView(this));
     }
 
 }
